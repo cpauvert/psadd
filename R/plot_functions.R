d93df7dc742bd61a9b2786a81fbba4ee802c0b5a
Charlie Pauvert <charlie.pauvert@inra.fr>
2017-01-23 09:28:36 +0100

    Add plot functions to evaluate sparsity and overdispersion

--- R/plot_functions.R
+++ R/plot_functions.R
@@ -72,3 +72,92 @@ plot_pie<-function(physeq, title.,condition,COL.TAXO. = COL.TAXO){
     outer=subset(outer_tax,Description==condition, select = 1,drop= TRUE))
   return(list_taxa)
 }
+
+#' Plot Mean Variance OTU in order to evaluate overdispersion
+#'
+#' The \code{\link{mean}} and variance (\code{\link{var}}) of \code{physeq} OTU table are computed.
+#' Overdispersion can be visually assessed by the generated \code{\link{plot}}. The putative linear relation
+#' between mean and variance is added with \code{\link{abline}} to guide the analysis.
+#'
+#' @param physeq \code{\link{phyloseq-class}}
+#' @param log A \code{\link{logical}} stating whether to apply \code{\link{log1p}} transform to both axis.
+#' @param title A title to the mean-variance plot
+#'
+#' @details The \code{\link{log1p}} transform was chosen to limit \code{\link{NaN}} values with null values.
+#'
+#' @references Modified from \href{https://doi.org/10.1186/s40168-016-0208-8}{Thorsen and Brejnrod et al. (2016)}.
+#'
+#' @return A \code{\link[ggplot2]{ggplot}} object
+#' @seealso \code{\link{taxa_stats}}
+#'
+#' @export
+#' @import ggplot2
+#' @import phyloseq
+#' @examples
+#' require(phyloseq)
+#' data(GlobalPatterns)
+#' (mv1<-plot_mv(GlobalPatterns, title = "Mean-Variance Plot with log-transform"))
+#' mv1.nolog<-plot_mv(GlobalPatterns,log = FALSE, title = "Mean-Variance Plot without log-transform")
+plot_mv<-function(physeq,log=TRUE,title = NULL){
+  p<-ggplot(
+    data.frame(
+      m=taxa_stats(physeq,mean),
+      v=taxa_stats(physeq,var)),
+    aes(x = m,y = v)) +
+    geom_point()+
+    geom_abline(intercept = 0,slope = 1)+
+    labs(x = "Mean", y = "Variance")
+  if(log){
+    p<-p+scale_y_continuous(trans = "log1p")+scale_x_continuous(trans = "log1p")
+  }
+  if (!is.null(title)) {
+    p <- p + ggtitle(title)
+  }
+  return(p)
+}
+
+
+#' Plot taxa/OTU table to evaluate sparsity.
+#'
+#'
+#' Taxa/OTU are ordered by their prevalence in the dataset, and samples are
+#' ordered by their richness (or completedness).
+#'
+#' @inheritParams plot_mv
+#'
+#' @references Adapted from \href{https://doi.org/10.1186/s40168-016-0208-8}{Thorsen and Brejnrod et al. (2016)}.
+#'
+#' @return A \code{\link[ggplot2]{ggplot}} object
+#' @seealso \code{\link{plot_mv}},\code{\link{taxa_prev}}, \code{\link{sample_prev}}
+#' @export
+#' @import phyloseq
+#' @import ggplot2
+#' @examples
+#' require(phyloseq)
+#' data(GlobalPatterns)
+#' (p<-plot_sparsity(GlobalPatterns, title = "Sparsity Plot"))
+plot_sparsity<-function(physeq, title = NULL){
+  # Melt the OTU table and merge associated metadata
+  df<-psmelt(physeq)[,1:3]# keep OTU, Sample, Abundance columns
+  # Order OTU by their occurrence/prevalence
+  df$OTU<-factor(x = df$OTU,
+                 levels = names(sort(taxa_prev(physeq))) )
+  # Order Samples by their richness (or completedness)
+  df$Sample<-factor(x = df$Sample,
+                    levels = names(sort(sample_prev(physeq))) )
+  # Transform Abundance numeric vector into boolean presence/absence
+  df$Abundance<-df$Abundance > 0
+  # Plot OTU table
+  p<-ggplot(df[df$Abundance,],
+            aes(x=Sample, y=OTU))+
+    geom_tile()+scale_x_discrete(drop = F) + scale_y_discrete(drop=F) +
+    theme_minimal()+theme(axis.text.x = element_blank(),
+                          axis.text.y = element_blank(),
+                          axis.ticks = element_blank())+
+    labs(x= paste(length(levels(df$Sample)),"samples - from low to high richness"),
+         y= paste(length(levels(df$OTU)),"OTU -- from rare to ubiquist"))
+  if (!is.null(title)) {
+    p <- p + ggtitle(title)
+  }
+  return(p)
+}
