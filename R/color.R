9cfee52dc0d2a72e70cdba002b10d5e3e82994d3
Charlie Pauvert <charlie.pauvert@inra.fr>
2017-01-24 13:12:29 +0100

    Added two INRA color palettes.

--- R/color.R
+++ R/color.R
@@ -16,22 +16,27 @@ COL.TAXO<-list(
             "#f1817f", "#2e4e4d", "#ff4700", "#9acd30", "#2191ff", "#9e0000",
             "#6b59cf", "#b2f0ef", "grey", "grey", "grey", "grey", "grey"))
 
+# INRA colors
+bleu <- rgb(42, 162, 224, maxColor = 255)  # light blue
+bleu2 <- rgb(42, 162, 224, 50, maxColor = 255)  # semitransparent light blue
+BLEU <- rgb(1, 63, 78, maxColor = 255)  # dark blue
+BLEU2 <- rgb(1, 63, 78, 50, maxColor = 255)  # semi transparent dark blue
+vert <- rgb(188, 215, 49, maxColor = 255)
+vert2 <- rgb(188, 215, 49, 50, maxColor = 255)
+VERT <- rgb(139, 172, 33, maxColor = 255)
+VERT2 <- rgb(139, 172, 33, 50, maxColor = 255)
+rose <- rgb(237, 26, 92, maxColor = 255)
+rose2 <- rgb(237, 26, 92, 50, maxColor = 255)
+ROSE <- rgb(123, 41, 70, maxColor = 255)
+ROSE2 <- rgb(123, 41, 70, 50, maxColor = 255)
+orange <- rgb(244, 125, 32, maxColor = 255)
+orange2 <- rgb(244, 125, 32, 50, maxColor = 255)
+ORANGE <- rgb(109, 70, 53, maxColor = 255)
+ORANGE2 <- rgb(109, 70, 53, 50, maxColor = 255)
+grey <- rgb(150, 150, 150, maxColor = 255)
+grey2 <- rgb(150, 150, 150, 50, maxColor = 255)
+
+
 # INRA color palette
-bleu <- rgb(42, 162, 224, maxColor= 255) # light blue
-bleu2 <- rgb(42, 162, 224, 50, maxColor= 255) # semitransparent light blue
-BLEU <- rgb(1, 63, 78, maxColor= 255)  # dark blue
-BLEU2 <- rgb(1, 63, 78, 50, maxColor= 255) # semi transparent dark blue
-vert <- rgb(188, 215, 49, maxColor= 255)
-vert2 <- rgb(188, 215, 49, 50, maxColor= 255)
-VERT <-  rgb(139, 172, 33, maxColor= 255)
-VERT2 <-  rgb(139, 172, 33, 50, maxColor= 255)
-rose <-  rgb(237, 26, 92, maxColor= 255)
-rose2 <-  rgb(237, 26, 92, 50, maxColor= 255)
-ROSE <-  rgb(123, 41, 70, maxColor= 255)
-ROSE2 <-  rgb(123, 41, 70, 50, maxColor= 255)
-orange <- rgb(244, 125, 32, maxColor= 255)
-orange2 <- rgb(244, 125, 32, 50, maxColor= 255)
-ORANGE <- rgb(109, 70, 53, maxColor= 255)
-ORANGE2 <- rgb(109, 70, 53, 50, maxColor= 255)
-grey <- rgb(150,150,150, maxColor= 255)
-grey2 <- rgb(150,150,150, 50, maxColor= 255)
+inra.palette<-c(bleu, vert, rose, orange, grey)
+inra.palette.dark<-c(BLEU, VERT, ROSE, ORANGE, grey)
